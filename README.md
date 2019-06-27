4d08eaaee1b5819b4a4cddb480acf75901f78bb2
Robert Vulpe <nytr0gen.george@gmail.com>
2019-06-27 21:34:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Deduplicate
+
+Parsing through long lists of endpoints, full of params, became a hassle? You want to get out? So no more, Deduplicate is the solution. With less than 200 lines of code, written in Go, it will remove any duplicates from your lists and make visual parsing a bliss.
+
+I made this so that I can go faster through [wayback urls](https://github.com/tomnomnom/waybackurls) output.
+
+## How does it work?
+
+Glad you asked. It formats an url to '%host%path%query_names', checks if the url was already found, if not, adds it into the map.
+
+## Install
+
+```bash
+go get github.com/nytr0gen/deduplicate
+```
+
+## Usage
+
+```bash
+echo domain | waybackurls | deduplicate --hide-images --sort
+```
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+    "bufio"
+    "flag"
+    "fmt"
+    "net/url"
+    "os"
+    "strings"
+    "sort"
+)
+
+func main() {
+    var urls []string
+
+    var hideImages bool
+    flag.BoolVar(&hideImages, "hide-images", false, "hide images")
+
+    var shouldSort bool
+    flag.BoolVar(&shouldSort, "sort", true, "sort output")
+
+    flag.Parse()
+
+    var file *os.File
+    if flag.NArg() > 0 {
+        // open file
+        var err error
+        file, err = os.Open(flag.Arg(0))
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        // fetch for all urls from stdin
+        file = os.Stdin
+    }
+
+    sc := bufio.NewScanner(file)
+    for sc.Scan() {
+        urls = append(urls, sc.Text())
+    }
+
+    if err := sc.Err(); err != nil {
+        panic(err)
+    }
+
+    found := make(map[string]string)
+    for _, uri := range urls {
+        uri = strings.TrimSpace(uri)
+        if uri == "" {
+            continue
+        }
+
+        u, _ := url.Parse(uri)
+        if u == nil {
+            continue
+        }
+
+        if hideImages {
+            pos := strings.LastIndexByte(u.Path, '.')
+            if pos != -1 {
+                ext := strings.ToLower(u.Path[pos+1:])
+                if extIsImage(ext) {
+                    continue
+                }
+            }
+        }
+
+        // ignore scheme, port, query values, auth info and hash
+        key := u.Host + u.Path
+        q := u.Query()
+        if len(q) != 0 {
+            key += "?"
+            for k, _ := range q {
+                key += k + "&"
+            }
+        }
+
+        if val, ok := found[key]; ok {
+            // prefer https urls
+            if u.Scheme == "https" && strings.HasPrefix(val, "http:") {
+                found[key] = uri
+            }
+        } else {
+            found[key] = uri
+        }
+    }
+
+    if !shouldSort {
+        for _, uri := range found {
+            fmt.Println(uri)
+        }
+    } else {
+        keys := make([]string, len(found))
+        i := 0
+        for k, _ := range(found) {
+            keys[i] = k
+            i++
+        }
+
+        sort.Strings(keys)
+        for _, k := range keys {
+            fmt.Println(found[k])
+        }
+    }
+}
+
+func extIsImage(ext string) bool {
+    return (ext == "png" ||
+        ext == "gif" ||
+        ext == "ico" ||
+        ext == "jpg" ||
+        ext == "jpeg")
+}
